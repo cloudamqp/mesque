98859ab6c5af7dc4eb501923deb2dc6861d58fe3
Carl Hörberg <carl.hoerberg@gmail.com>
2014-03-25 16:24:31 +0800

    clean up in mesque#work

--- lib/mesque.rb
+++ lib/mesque.rb
@@ -12,20 +12,23 @@ class Mesque
   end
 
   def <<(obj)
-    vars = obj.instance_variables.map { |k| [k, obj.instance_variable_get(k).sub(/^@/, '')] }
+    vars = obj.instance_variables.map do |k|
+      [ k[1..-1], obj.instance_variable_get(k) ]
+    end
     pub(class: obj.class, vars: Hash[vars])
   end
 
   def work(queue = 'mesque')
     @bunny.with_channel do |ch|
       ch.qos 20
-      ch.subscribe(queue, ack: true, block: true) do |delivery, props, body|
+      q = ch.queue queue, durable: true
+      q.subscribe(ack: true, block: true) do |delivery, props, payload|
         begin
-          json = JSON.parse msg, symbolize_keys: true
-          if json[:args]
+          json = JSON.parse payload, symbolize_names: true
+          if json[:args] # Resque 1.x and Sidekiq style
             job = json[:class].constantize
-            job.perform(*json[:args]) 
-          elsif json[:vars]
+            job.perform(*json[:args])
+          elsif json[:vars] # Resque 2.0 style
             job = json[:class].constantize.new
             json[:vars].each do |k, v|
               job.instance_variable_set("@#{k}", v)
@@ -34,7 +37,8 @@ class Mesque
           end
           ch.acknowledge(delivery.delivery_tag, false)
         rescue Exception => e
-          puts "[ERROR] #{e}"
+          puts "[ERROR] Mesque failed with: #{payload}"
+          puts "#{e.inspect}\n  #{e.backtrace.join("\n  ")}"
           ch.reject(delivery.delivery_tag, true)
         end
       end
@@ -43,9 +47,7 @@ class Mesque
 
   private
   def pub(msg)
-    @q.publish(msg.to_json, 
-               content_type: 'application/json',
-               persistent: true)
+    @q.publish(msg.to_json, content_type: 'application/json', persistent: true)
   end
 end
 
